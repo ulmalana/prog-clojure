19e186da95ecb3f1492cad163f77fac9c1a558cd
rz <muhamaul@gmail.com>
2023-01-21 14:12:04 +0700

    finished chapter 9

--- ch09-multimethod/src/ch09_multimethod/core.clj
+++ ch09-multimethod/src/ch09_multimethod/core.clj
@@ -107,3 +107,132 @@
 
 (defmethod my-print-multi :everything-else [_]
   (.write *out* "Not implemented yet..."))
+
+
+;; print list
+(defmethod my-print-multi java.util.Collection [c]
+  (.write *out* "(")
+  (.write *out* (str/join " " c))
+  (.write *out* ")"))
+
+(my-println (take 6 (cycle [1 2 3])))
+;; (1 2 3 1 2 3)
+;; => nil
+
+(my-println [1 2 3])
+;; (1 2 3)
+;; => nil
+
+;; print vector
+(defmethod my-print-multi clojure.lang.IPersistentVector [c]
+  (.write *out* "[")
+  (.write *out* (str/join " " c))
+  (.write *out* "]"))
+
+;; will throw exception since dispatch values match collection and IPersistentVector
+(comment
+  (my-println [1 2 3])
+  ;; Execution error (IllegalArgumentException) at ch09-multimethod.core/my-println (core.clj:57).
+  ;; Multiple methods in multimethod 'my-print-multi' match dispatch value: class clojure.lang.PersistentVector -> interface clojure.lang.IPersistentVector and interface java.util.Collection, and neither is preferred
+  )
+
+;; in case of such conflict,
+;; tell clojure to prefer specific method
+(prefer-method my-print-multi
+               clojure.lang.IPersistentVector
+               java.util.Collection)
+
+(my-println (take 6 (cycle [1 2 3])))
+;; (1 2 3 1 2 3)
+;; => nil
+
+(my-println [1 2 3])
+;; [1 2 3]
+;; => nil
+
+;;; ad hoc taxonomy
+(def test-savings {:id 1, :tag ::savings, ::balance 100M})
+;; => #'ch09-multimethod.core/test-savings
+
+(def test-checking {:id 2, :tag ::checking, ::balance 250M})
+;; => #'ch09-multimethod.core/test-checking
+
+(defmulti interest-rate :tag)
+(defmethod interest-rate ::checking [_]
+  0M)
+
+(defmethod interest-rate ::savings [_]
+  0.05M)
+
+(interest-rate test-savings)
+;; => 0.05M
+
+(interest-rate test-checking)
+;; => 0M
+
+(defmulti account-level :tag)
+(defmethod account-level ::checking [acct]
+  (if (>= (:balance acct) 5000)
+    ::premium
+    ::basic))
+(defmethod account-level ::savings [acct]
+  (if (>= (:balance acct) 1000)
+    ::premium
+    ::basic))
+
+(account-level {:id 1 :tag ::savings :balance 2000M})
+;; => :ch09-multimethod.core/premium
+
+(account-level {:id 1 :tag ::checking :balance 2000M})
+;; => :ch09-multimethod.core/basic
+
+;; bad approach
+(comment
+  (defmulti service-charge account-level)
+  (defmethod service-charge ::basic [acct]
+    (if (= (:tag acct) ::checking) 25 10))
+  (defmethod service-charge ::premium [_] 0)
+  )
+
+;; better
+(defmulti service-charge (fn [acct]
+                           [(account-level acct) (:tag acct)]))
+
+(defmethod service-charge [::basic ::checking] [_]
+  25)
+
+(defmethod service-charge [::basic ::savings] [_]
+  20)
+
+(defmethod service-charge [::premium ::checking] [_]
+  0)
+
+(defmethod service-charge [::premium ::savings] [_]
+  0)
+
+(service-charge {:tag ::checking :balance 1000})
+;; => 25
+
+(service-charge {:tag ::savings :balance 1000})
+;; => 0
+
+;; simplifying multimethod with inheritance
+;; specify ::savings and ::checking as kind of ::account
+(derive ::savings ::account)
+(derive ::checking ::account)
+
+(isa? ::savings ::account)
+;; => true
+
+;; simplified
+(defmulti service-charge (fn [acct]
+                           [(account-level acct) (:tag acct)]))
+
+(defmethod service-charge [::basic ::checking] [_]
+  25)
+
+(defmethod service-charge [::basic ::savings] [_]
+  20)
+
+(defmethod service-charge [::premium ::account] [_]
+  0)
